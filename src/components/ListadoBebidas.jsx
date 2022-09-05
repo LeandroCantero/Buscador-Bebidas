118221e061c745bdfb79a6f074fbb86d945172af
LeandroCantero <lcantero18@gmail.com>
2022-09-05 19:18:06 -0300

    End

---
+++ src/components/ListadoBebidas.jsx
@@ -0,0 +1,22 @@
+import useBebidas from "../hooks/useBebidas"
+import Row from "react-bootstrap/esm/Row"
+import Bebida from "./Bebida"
+
+function ListadoBebidas() {
+
+    const { bebidas } = useBebidas()
+
+    return (
+        <Row className="mt-5">
+            {bebidas.map(bebida => (
+                <Bebida
+                    key={bebida.idDrink}
+                    bebida={bebida}
+                    
+                />
+            ))}
+        </Row>
+    )
+}
+
+export default ListadoBebidas
